b8a0c85ea8d72766dd6abcc1c215f23c48000d2a
sonnyshih <sonnyshih@gmail.com>
2014-12-12 09:25:02 +0800

    N/A

--- src/com/example/CustomerUIDemo/activity/TabHost/AndroidFragment.java
+++ src/com/example/CustomerUIDemo/activity/TabHost/AndroidFragment.java
@@ -5,6 +5,9 @@ import com.example.CustomerUIDemo.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -13,6 +16,7 @@ public class AndroidFragment extends Fragment{
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);	// Set the option menu
 	}
     
 	@Override
@@ -21,6 +25,28 @@ public class AndroidFragment extends Fragment{
 		View view = inflater.inflate(R.layout.android_fragement, container, false);
 		
 		return view;
+	}
+	
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+	    super.onCreateOptionsMenu(menu, inflater);
+	    inflater.inflate(R.menu.context_menu, menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+//	    switch (item.getItemId()) {
+//	    case R.id.activity_menu_item:
+//	        // Not implemented here
+//	        return false;
+//	    case R.id.fragment_menu_item:
+//	        // Do Fragment menu item stuff here
+//	        return true;
+//	    default:
+//	        break;
+//	    }
+
+	    return false;
 	}
 	
 }
